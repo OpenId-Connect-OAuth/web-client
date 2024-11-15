1384305756f5f85f5732057de49d1a90c9f1fbca
vikkastaneja <vikkastaneja@users.noreply.github.com>
2024-11-14 22:50:22 -0800

    Login to auth server to get ID Token

--- src/main/java/com/vksdeveloperblog/ws/clients/photowebclient/controllers/AlbumsController.java
+++ src/main/java/com/vksdeveloperblog/ws/clients/photowebclient/controllers/AlbumsController.java
@@ -3,6 +3,9 @@ package com.vksdeveloperblog.ws.clients.photowebclient.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +16,11 @@ import com.vksdeveloperblog.ws.clients.photoappwebclient.response.AlbumRest;
 public class AlbumsController {
 
     @GetMapping("/albums")
-    public String getAlbums(Model model) {
+    public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
+        System.out.println("Principal: " + principal);
+
+        OidcIdToken uIdToken = principal.getIdToken();
+        System.out.println("Token: " + uIdToken.getTokenValue());
         List<AlbumRest> albums = new ArrayList<AlbumRest>(){
             {
                 add(new AlbumRest().setAlbumId("1").setAlbumTitle("Title 1").setAlbumUrl("http://localhost:8082/albums/1"));
