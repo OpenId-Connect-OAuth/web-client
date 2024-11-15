86899d21477617383289b5189d5094c1f15ce1c1
vikkastaneja <vikkastaneja@users.noreply.github.com>
2024-11-14 23:04:20 -0800

    Retrieve JWT Access Token

--- src/main/java/com/vksdeveloperblog/ws/clients/photowebclient/controllers/AlbumsController.java
+++ src/main/java/com/vksdeveloperblog/ws/clients/photowebclient/controllers/AlbumsController.java
@@ -3,7 +3,13 @@ package com.vksdeveloperblog.ws.clients.photowebclient.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -15,8 +21,19 @@ import com.vksdeveloperblog.ws.clients.photoappwebclient.response.AlbumRest;
 @Controller
 public class AlbumsController {
 
+    @Autowired
+    OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
     @GetMapping("/albums")
     public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
+        OAuth2AuthenticationToken oauthtoken = (OAuth2AuthenticationToken)SecurityContextHolder.getContext().getAuthentication();
+
+        OAuth2AuthorizedClient oauth2Client = oAuth2AuthorizedClientService.loadAuthorizedClient(oauthtoken.getAuthorizedClientRegistrationId(), oauthtoken.getName());
+
+        String jwtAccessToken = oauth2Client.getAccessToken().getTokenValue();
+
+        System.out.println("JWT Access Toke: " + jwtAccessToken);
+
         System.out.println("Principal: " + principal);
 
         OidcIdToken uIdToken = principal.getIdToken();
